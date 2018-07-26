321393d677f549c489ea08fd64215a12bc511b0f
LoganBrannan <loganbrannan13@gmail.com>
2018-07-26 12:54:34 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,9 +1,13 @@
 
 def create_olympics_hash
-  # Implement this method so that it returns a hash with the data provided on README.md
+  
+ hash = {:Sydney => ("2000"), :Athens => ("2004"), :Beijing => ("2008"), :London => ("2012")} 
 end
 
 def add_a_key_value_pair
+  hash = {:Sydney => ("2000"), :Athens => ("2004"), :Beijing => ("2008"), :London => ("2012")} 
+ hash[:Atlanta] = 1996
+ return hash
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
 end
 
